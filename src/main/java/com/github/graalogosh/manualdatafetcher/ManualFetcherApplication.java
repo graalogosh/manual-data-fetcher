27db002d9f66b987fbb0dead32d672efc3f35da2
Anton Mukovozov <graalogosh@gmail.com>
2018-11-03 00:27:35 +0300

    Initial commit

---
+++ src/main/java/com/github/graalogosh/manualdatafetcher/ManualFetcherApplication.java
@@ -0,0 +1,12 @@
+package com.github.graalogosh.manualdatafetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManualFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManualFetcherApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/graalogosh/manualdatafetcher/ManualFetcherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.graalogosh.manualdatafetcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManualFetcherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
